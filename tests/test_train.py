4fe2b3fbe7c20c2fe60704676a834a93b061282b
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2024-05-16 16:57:05 +0200

    test train fin 42 16 may

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,11 +1,14 @@
 import unittest
 from src.Train import MyLeafCNN
 from src.Train import normalise_img, create_zip
+import numpy as np
+
 
 class TestTrainMyLeafCNN(unittest.TestCase):
     """Test for the trainning of my CNN Model for Leaf identifiaction"""
 
     def setUp(self) -> None:
+        
         return super().setUp()
 
 
@@ -15,10 +18,14 @@ class TestTrainMyLeafCNN(unittest.TestCase):
 
     def test_normalise_img(self):
         """ Test the normilise img function"""
+        img = np.random.randint(255, size=(256, 256, 3), dtype=np.uint8)
+        norm_img = normalise_img(img)
+        self.assertTrue(np.max(norm_img.numpy()) <= 1.0)
 
 
-    def test_load_dataset(self):
+    def test_load_dataset(self) -> int:
         """ Test the load_dataset function"""
+        return ""
 
 
     def test_create_zip_file(self):
@@ -35,3 +42,7 @@ class TestTrainMyLeafCNN(unittest.TestCase):
 
     def test_train_model(self):
         """Test the train function"""
+
+
+if __name__ == "__main__":
+    unittest.main()
